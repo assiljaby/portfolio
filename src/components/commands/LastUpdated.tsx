4cc150930c5cab1a962d379383a51e97051d7ad0
Assil Jaby <altf4irl@gmail.com>
2025-02-09 22:35:22 +0300

    fix: simple mistake hehe

--- src/components/commands/LastUpdated.tsx
+++ src/components/commands/LastUpdated.tsx
@@ -6,7 +6,7 @@ const LastUpdated: React.FC = () => {
     return (
         <Wrapper>
             <GeneralOutput>Feb 9, 2025 - 18:20:00UTC</GeneralOutput>
-            <i>Disclamer: The author is suffering from ADHD and might've <br /> forgot to update this section.</i>
+            <i>Disclamer: The author is suffering from ADHD and might've <br /> forgotten to update this section.</i>
         </Wrapper>
     )
 };
